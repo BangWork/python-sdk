f259f7b5cd94ea12e488d83890c495352b421074
pantalo <pantaloa@gmail.com>
2018-03-30 12:24:25 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,25 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='ones',
     version='0.1.0',
     description='ONES.AI SDK',
     long_description='see:\nhttps://github.com/BangWork/python-sdk\n',
+    url='https://github.com/BangWork/python-sdk',
     author='ONES.AI',
     author_email='dev@ones.ai',
-    maintainer_email='dev@ones.ai',
+    packages=['ones'],
     license='Apache License 2.0',
-    url='https://github.com/BangWork/python-sdk',
-    platforms='any',
-    packages=[
-        'ones'
-    ],
     classifiers=[
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
+    ]
 )
